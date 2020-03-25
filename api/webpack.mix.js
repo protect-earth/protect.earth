ddaa2fc17798a3db668632c3a6fed5b9d7e6c5fc
Phil Sturgeon <me@philsturgeon.uk>
2020-03-25 11:29:52 +0000

    initial statamic setup

---
+++ api/webpack.mix.js
@@ -0,0 +1,29 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/site.js', 'public/js')
+   .sass('resources/sass/site.scss', 'public/css');
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Statamic Control Panel Assets
+ |--------------------------------------------------------------------------
+ |
+ | Feel free to add your own JS or CSS to the Statamic Control Panel.
+ | https://statamic.dev/extending/control-panel#adding-css-and-js-assets
+ |
+ */
+
+// mix.js('resources/js/cp.js', 'public/vendor/app/js')
+//    .sass('resources/sass/cp.scss', 'public/vendor/app/css');
