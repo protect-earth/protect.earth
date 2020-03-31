d8c9d5ac06554d9bf745e87d1a82c8250aa01d32
Joao Patricio <ijpatricio@gmail.com>
2020-04-01 00:30:56 +0100

    Adds favicon. Do not ignore public folder.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app" class="flex flex-col items-center">
-    <img class="max-h-full max-w-full" alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img class="max-h-full max-w-full" alt="Vue logo" src="./assets/hero-bg.jpg">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
