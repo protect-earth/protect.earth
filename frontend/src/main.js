be44fc09441608ee924fd2195b0223ae9e9743b1
Joao Patricio <ijpatricio@gmail.com>
2020-04-01 00:50:22 +0100

    Installs vue-router, vuex, vue-meta, vue-analytics. Scaffolds first view.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import router from './router'
+import './plugins/meta'
 import App from './App.vue'
 import './assets/main.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ frontend/src/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  scrollBehavior: () => ({y: 0}),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import(/* webpackChunkName: "home" */ './views/home/Index'),
+    },
+  ],
+})
+
+// Bootstrap Analytics
+// Set in .env
+// https://github.com/MatteoGabriele/vue-analytics
+if (process.env.VUE_APP_GOOGLE_ANALYTICS) {
+  Vue.use(require('vue-analytics').default, {
+    id: process.env.VUE_APP_GOOGLE_ANALYTICS,
+    router,
+    autoTracking: {
+      page: process.env.NODE_ENV !== 'development',
+    },
+  })
+}
+
+export default router
