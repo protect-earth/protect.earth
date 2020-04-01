ce417cb3f655e4f87ee39eccff9a10d58611df6e
Joao Patricio <ijpatricio@gmail.com>
2020-04-01 01:57:54 +0100

    Starts components structure.

---
+++ frontend/src/components/Article.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Article body
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Article"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/plugins/base.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Card from '@/components/base/Card'
+
+Vue.component('BaseCard', Card)
+
+// @TODO is globally registering needed?
+// Vue.component('ActionListing', ActionListing)
+// Vue.component('Article', Article)
