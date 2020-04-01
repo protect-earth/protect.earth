2fb8aec66b1a9f92f325c2ce6af23a73f562a948
Squallac <squall_ff@hotmail.com>
2020-04-01 09:37:51 -0600

    Adding basic tags functionality

--- src/router.js
+++ src/router.js
@@ -4,29 +4,36 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 const router = new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  scrollBehavior: () => ({y: 0}),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import(/* webpackChunkName: "home" */ './views/home/Index'),
-    },
-  ],
+    mode: 'history',
+    base: process.env.BASE_URL,
+    scrollBehavior: () => ({ y: 0 }),
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: () =>
+                import(/* webpackChunkName: "home" */ './views/home/Index'),
+        },
+        {
+            path: '/tags/:tagName',
+            name: 'tags',
+            component: () =>
+                import(/* webpackChunkName: "home" */ './views/tags/TagsIndex'),
+        },
+    ],
 })
 
 // Bootstrap Analytics
 // Set in .env
 // https://github.com/MatteoGabriele/vue-analytics
 if (process.env.VUE_APP_GOOGLE_ANALYTICS) {
-  Vue.use(require('vue-analytics').default, {
-    id: process.env.VUE_APP_GOOGLE_ANALYTICS,
-    router,
-    autoTracking: {
-      page: process.env.NODE_ENV !== 'development',
-    },
-  })
+    Vue.use(require('vue-analytics').default, {
+        id: process.env.VUE_APP_GOOGLE_ANALYTICS,
+        router,
+        autoTracking: {
+            page: process.env.NODE_ENV !== 'development',
+        },
+    })
 }
 
 export default router
