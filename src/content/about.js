339132ddb742f3f4b786dcf68a522e6944518c3f
ijpatricio <ijpatricio@users.noreply.github.com>
2020-04-08 23:27:50 +0100

    Feature/pages content (#44)
    * Step towards local content.
    
    * Step towards local content.
    
    * Removes clutter. Drops centered text. Adds utility to links.

---
+++ src/content/about.js
@@ -0,0 +1,20 @@
+const htmlContent = `
+    <p class="mb-4 text-lg leading-relaxed text-gray-800">
+      Protect.Earth came around after <a href="https://philsturgeon.com/">Phil</a> went riding around
+      Europe in the summer of 2019, the hottest year on record with the hottest three months on record.
+    </p>
+    <p class="mb-4 text-lg leading-relaxed text-gray-800">
+      Phil was <a href="https://phil.bike/trees/">raising money for trees</a> with
+      <a href="https://offset.earth/">Offset Earth</a>, and after the tour lead to a lengthy injury he
+      wanted to find other ways to fight the climate crisis.
+    </p>
+    <p class="mb-4 text-lg leading-relaxed text-gray-800">Together with contributions from 50+ people, we
+      gather resources to help everyone find their next
+      piece of climate action. The climate crisis might be overwhelming when you start to understand the
+      severity of it all, but if we can get people on board with simplifying their lives, reducing their
+      footprint, offsetting the remaining footprint for barely any money, and reducing the insane
+      amounts of waste we all cause, our kids might have a chance.
+    </p>
+  `
+
+export default htmlContent
