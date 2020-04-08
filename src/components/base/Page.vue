0da5bdc89524700abef63144b22427ab2194f003
Joao Patricio <ijpatricio@gmail.com>
2020-04-08 18:58:19 +0100

    Step towards local content.

--- src/components/base/Page.vue
+++ src/components/base/Page.vue
@@ -23,27 +23,6 @@
                                            style="max-width: 150px;">
                 </div>
               </div>
-              <div class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                <div class="py-6 px-3 mt-32 sm:mt-0">
-                  <button
-                    class="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
-                    type="button" style="transition: all 0.15s ease 0s;">Connect
-                  </button>
-                </div>
-              </div>
-              <div class="w-full lg:w-4/12 px-4 lg:order-1">
-                <div class="flex justify-center py-4 lg:pt-4 pt-8">
-                  <div class="mr-4 p-3 text-center"><span
-                    class="text-xl font-bold block uppercase tracking-wide text-gray-700">22</span><span
-                    class="text-sm text-gray-500">Friends</span></div>
-                  <div class="mr-4 p-3 text-center"><span
-                    class="text-xl font-bold block uppercase tracking-wide text-gray-700">10</span><span
-                    class="text-sm text-gray-500">Photos</span></div>
-                  <div class="lg:mr-4 p-3 text-center"><span
-                    class="text-xl font-bold block uppercase tracking-wide text-gray-700">89</span><span
-                    class="text-sm text-gray-500">Comments</span></div>
-                </div>
-              </div>
             </div>
             <div class="text-center mt-12"><h3 class="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
               About</h3>
@@ -60,28 +39,8 @@
             </div>
             <div class="mt-10 py-10 border-t border-gray-300 text-center">
               <div class="flex flex-wrap justify-center">
-                <div class="w-full lg:w-9/12 px-4">
-
-                  <p class="mb-4 text-lg leading-relaxed text-gray-800">
-                    Protect.Earth came around after
-                    <a href="https://philsturgeon.com/">Phil</a>
-                    went riding
-                    around Europe in the summer of 2019, the hottest year on record with the hottest three months on
-                    record.
-                  </p>
-                  <p class="mb-4 text-lg leading-relaxed text-gray-800">
-                    Phil was <a href="https://phil.bike/trees/">raising money for trees</a> with <a
-                    href="https://offset.earth/">Offset Earth</a>, and after the tour lead to a lengthy injury he
-                    wanted to find other ways to fight the climate crisis.
-                  </p>
-                  <p class="mb-4 text-lg leading-relaxed text-gray-800">Together with contributions from 50+ people, we
-                    gather resources to help everyone find their next
-                    piece of climate action. The climate crisis might be overwhelming when you start to understand the
-                    severity of it all, but if we can get people on board with simplifying their lives, reducing their
-                    footprint, offsetting the remaining footprint for barely any money, and reducing the insane
-                    amounts of waste we all cause, our kids might have a chance.</p>
-
-                  <a href="#pablo" class="font-normal text-pink-500">Show more</a></div>
+                <div class="w-full lg:w-9/12 px-4" v-html="content">
+                </div>
               </div>
             </div>
           </div>
