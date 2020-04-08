c5f65a09dc9f43e2a9d16eba107e4a34284a0727
Joao Patricio <ijpatricio@gmail.com>
2020-04-08 18:53:32 +0100

    Step towards local content.

---
+++ src/content/about.js
@@ -0,0 +1,9 @@
+const htmlContent = `
+<p>
+    This is some awesome content!!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto cupiditate ea id illum ipsa iure labore,
+    mollitia nemo nesciunt nisi, porro quaerat quis quisquam quod repudiandae tempore unde, vero voluptatem?
+</p>
+`
+
+export default htmlContent
