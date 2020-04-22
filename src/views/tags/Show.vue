b23a562cd2baf7261c39de1ac6849942aee49f39
Squallac <squall_ff@hotmail.com>
2020-04-22 10:11:29 -0500

    Fixing errors in console

--- src/views/tags/Show.vue
+++ src/views/tags/Show.vue
@@ -1,37 +1,38 @@
 <template>
   <div class="text-gray-800 antialiased">
-
     <navigation />
 
     <tags-main v-bind:tag-name="tagName" />
 
     <page-footer />
-
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'TagShow',
+export default {
+  name: 'TagShow',
 
-    metaInfo() {
-      return {
-        title: `Links tagged with ${this.tagSlug}`,
-      }
-    },
+  metaInfo() {
+    return {
+      title: `Links tagged with ${this.tagSlug}`
+    }
+  },
 
-    components: {
-      Navigation: () => import('./../../components/Navigation'),
-      PageFooter: () => import('./../../components/Footer'),
-      TagsMain: () => import('./TagsMain'),
-    },
+  components: {
+    Navigation: () => import('./../../components/Navigation'),
+    PageFooter: () => import('./../../components/Footer'),
+    TagsMain: () => import('./TagsMain')
+  },
 
-    data: () => ({
-      publicPath: process.env.BASE_URL,
-    }),
+  data: () => ({
+    tagName: '',
+    publicPath: process.env.BASE_URL
+  }),
 
-    computed: {
-      tagSlug: () => this.$route.params.tagSlug,
-    },
+  computed: {
+    tagSlug: function() {
+      return this.$route.params.tagSlug
+    }
   }
+}
 </script>
