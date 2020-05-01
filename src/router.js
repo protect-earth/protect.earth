96149d1564d826384f1339f6bf21520a748e81fd
imkieransmith <kieran@imkieransmith.com>
2020-05-01 22:12:00 +0100

    Add remaining categories

--- src/router.js
+++ src/router.js
@@ -65,6 +65,74 @@ let categories = [
     {
         id: 'f92ca585-ad4d-43bc-9430-43c2fad14aa1',
         slug: 'clothing'
+    },
+    {
+        id: 'a68b7a57-c0a5-4b27-81d2-93a19f2787a1',
+        slug: 'consumer-products'
+    },
+    {
+        id: 'e71796f8-4b3d-4f40-a1a8-527fb0fdf854',
+        slug: 'cooking'
+    },
+    {
+        id: '49f0ae64-b03a-4d50-bbdc-edd765ef4500',
+        slug: 'documentaries'
+    },
+    {
+        id: '1e06ea25-373d-440c-9abd-408710b475d0',
+        slug: 'food'
+    },
+    {
+        id: '681bffaf-a44c-4449-ae96-bf780506c862',
+        slug: 'footprint-calculators'
+    },
+    {
+        id: '6ad9cfc5-eac0-455e-9ad0-f537896373ba',
+        slug: 'home-improvements'
+    },
+    {
+        id: '7adab10c-985b-42e2-ab8c-eee35b5a8817',
+        slug: 'hygiene'
+    },
+    {
+        id: '1fcc2840-32ba-44fb-9b99-efe4d1397ff4',
+        slug: 'jobs'
+    },
+    {
+        id: '411e32f8-59bc-4fbb-ac7f-3d2a908b039e',
+        slug: 'land-management'
+    },
+    {
+        id: '0ec6e5b5-0a80-4c8d-b45f-b78c99492d8d',
+        slug: 'news'
+    },
+    {
+        id: '3d78ba9a-4f85-464b-a330-1cfb5c137328',
+        slug: 'politics'
+    },
+    {
+        id: '63a7cfb3-7cd5-4282-af9d-e5ed41572d1b',
+        slug: 'reforestation'
+    },
+    {
+        id: 'f9b2c5ee-8da3-446b-b865-0d716debed30',
+        slug: 'renewable-energy'
+    },
+    {
+        id: '0a32cb28-6330-4881-8671-824476ed5859',
+        slug: 'transportation'
+    },
+    {
+        id: '97826809-ed97-424c-9c46-cedba824add8',
+        slug: 'travel'
+    },
+    {
+        id: 'ee42a632-ac6a-4f89-802a-8111cf674d4c',
+        slug: 'volunteering'
+    },
+    {
+        id: 'a1a4ac88-627d-4bc7-a5b5-d3dcdc10cc43',
+        slug: 'waste'
     }
 ];
 
