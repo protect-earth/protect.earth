5c5a388e6db35bf50fa91c14b2b712904418cb94
Piper <piperchester@gmail.com>
2020-11-07 16:13:24 -0800

    fix(*): default selection logic

--- src/components/CountrySelection.jsx
+++ src/components/CountrySelection.jsx
@@ -7,28 +7,27 @@ import Countries from '../countries';
  * If the user has a country preference in session storage, we don't need to
  * present the user to select a country. We render a button to _clear_ their selection.
  */
-const CountryButton = ({ }) => {
-    // get the country code
-  const [country] = useState(sessionStorage.getItem('country_pref'))
+const CountryButton = ({}) => {
+  // Get the country code. e.g., "BG" for "Bulgaria"
+  const [country] = useState(sessionStorage.getItem('country_pref'));
 
-  const countryName = Countries.fromAlpha2Code(country).name;
-  const countryEmoji = Countries.fromAlpha2Code(country).emoji;
+  // Default to Great Britain
+  const countryName = Countries.fromAlpha2Code(country || 'GB').name;
+  const countryEmoji = Countries.fromAlpha2Code(country || 'GB').emoji;
 
   return (
     <div className="change-country">
-        {
-            country ? (
-            <>
-                <span className="current selected-item">
-                    {countryName}{' '}
-                    {countryEmoji}
-                </span>{' '}
-            </>
-            ) : null
-        }
-        <Button href="/select-your-country" className="link text-white">
-        {country ? 'Change' : 'Filter site for your'} country
-        </Button>
+      {countryName && countryEmoji ? (
+        <>
+          <span className="current selected-item">
+            {countryName} {countryEmoji}
+          </span>{' '}
+        </>
+      ) : null}
+
+      <Button href="/select-your-country" className="link text-white">
+        {countryName ? 'Change' : 'Filter site for your'} country
+      </Button>
     </div>
   );
 };
